17b0472b38d5b7a488ce265750709afdceeed687
Ian Dees <ian.dees@gmail.com>
2011-03-26 12:02:47 -0500

    Reorganize the icon mapings.

--- src/com/yellowbkpk/osmnearby/IconLookup.java
+++ src/com/yellowbkpk/osmnearby/IconLookup.java
@@ -28,18 +28,24 @@ public class IconLookup {
 
     static {
         addMapping(R.drawable.transport_fuel, Rule.n("amenity", "fuel"));
+        
         addMapping(R.drawable.amenity_firestation2, Rule.n("amenity", "fire_station"));
+        
         addMapping(R.drawable.education_university, Rule.n("amenity", "university"));
-        addMapping(R.drawable.food_restaurant, Rule.n("amenity", "restaurant"));
-        addMapping(R.drawable.place_of_worship, Rule.n("amenity", "place_of_worship"));
-        addMapping(R.drawable.place_of_worship_christian, Rule.n("amenity", "place_of_worship"), Rule.n("religion", "christian"));
-        addMapping(R.drawable.accommodation_hotel, Rule.n("tourism", "hotel"));
-        addMapping(R.drawable.food_cafe, Rule.n("amenity", "cafe"));
         addMapping(R.drawable.education_school, Rule.n("amenity", "school"));
+        
+        addMapping(R.drawable.food_restaurant, Rule.n("amenity", "restaurant"));
+        addMapping(R.drawable.food_cafe, Rule.n("amenity", "cafe"));
         addMapping(R.drawable.food_pub, Rule.n("amenity", "pub"));
-        addMapping(R.drawable.food_bar, Rule.n("amenity", "fast_food"));
+        addMapping(R.drawable.food_bar, Rule.n("amenity", "bar"));
         addMapping(R.drawable.food_fastfood2, Rule.n("amenity", "fast_food"), Rule.n("cuisine", "burger"));
         addMapping(R.drawable.food_pizza, Rule.n("amenity", "fast_food"), Rule.n("cuisine", "pizza"));
+        addMapping(R.drawable.food_fastfood, Rule.n("amenity", "fast_food"));
+        
+        addMapping(R.drawable.place_of_worship, Rule.n("amenity", "place_of_worship"));
+        addMapping(R.drawable.place_of_worship_christian, Rule.n("amenity", "place_of_worship"), Rule.n("religion", "christian"));
+        
+        addMapping(R.drawable.accommodation_hotel, Rule.n("tourism", "hotel"));
     }
 
     public static int forTags(Map<String, String> tags) {
